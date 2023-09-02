296e3a3d3ae79568d886d37346ae4ed65bba0931
egentilucci <elia.gentilucci@gmail.com>
2023-09-02 22:11:28 +0200

    Update: add array to endpoint

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 
+const stringArray = ["String 1", "String 2", "String 3"];
+
 app.get("/", (req, res) => {
-  res.json({ message: "Hello, World!" });
+  res.json(stringArray);
 });
 
 const PORT = process.env.PORT || 3000;
