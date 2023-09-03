5ddbcd071555631c3a1ce8f00495383dd92c21dc
egentilucci <elia.gentilucci@gmail.com>
2023-09-03 09:34:17 +0200

    feat: add reading data from endpoint (local)

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,40 @@
 <template>
   <div>
-    <Spinner />
+    <Spinner :options="options" />
   </div>
 </template>
 
-<script setup lang="ts">
+<script>
 import Spinner from "./components/Spinner.vue";
+import { ref, onMounted } from "vue";
+import axios from "axios";
+
+export default {
+  components: {
+    Spinner, // Register the spinner component
+  },
+  data() {
+    return {
+      options: [], // Initialize an empty array to store the data
+      loading: false,
+      error: null,
+    };
+  },
+  mounted() {
+    this.loading = true;
+    axios
+      .get("http://localhost:3000/api/options")
+      .then((response) => {
+        this.options = response.data;
+        this.loading = false;
+      })
+      .catch((error) => {
+        console.error("Error fetching options:", error);
+        this.error = error;
+        this.loading = false;
+      });
+  },
+};
 </script>
 
 <style scoped></style>
